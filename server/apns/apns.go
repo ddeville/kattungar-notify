54e22812f208f53763cac6265722de94351e11ae
Damien Deville <damien@ddeville.me>
2024-01-02 20:58:42 -0800

    Move consts to entry point and keep module generic

--- server/apns/apns.go
+++ server/apns/apns.go
@@ -8,34 +8,39 @@ import (
 	"github.com/ddeville/kattungar_notify/store"
 )
 
-const teamId = "Q8B696Y8U4"
-const keyId = "SZQY3SP3XB"
-const appId = "com.ddeville.kattungar_notify"
+type ApnsConfig struct {
+	TeamId  string
+	AppId   string
+	KeyId   string
+	KeyPath string
+}
 
 type ApnsClient struct {
 	inner *apns2.Client
+	cfg   ApnsConfig
 }
 
-func NewApnsClient(keyPath string) (*ApnsClient, error) {
-	authKey, err := token.AuthKeyFromFile(keyPath)
+func NewApnsClient(cfg ApnsConfig) (*ApnsClient, error) {
+	authKey, err := token.AuthKeyFromFile(cfg.KeyPath)
 	if err != nil {
 		return nil, err
 	}
 
 	token := &token.Token{
 		AuthKey: authKey,
-		KeyID:   keyId,
-		TeamID:  teamId,
+		KeyID:   cfg.KeyId,
+		TeamID:  cfg.TeamId,
 	}
 
 	client := apns2.NewTokenClient(token)
-	return &ApnsClient{client}, nil
+	return &ApnsClient{client, cfg}, nil
 }
 
 func (c *ApnsClient) Push(device store.Device, payload *payload.Payload) (*apns2.Response, error) {
-	notification := &apns2.Notification{}
-	notification.Topic = appId
-	notification.DeviceToken = device.Token
-	notification.Payload = payload
+	notification := &apns2.Notification{
+		Topic:       c.cfg.AppId,
+		DeviceToken: device.Token,
+		Payload:     payload,
+	}
 	return c.inner.Push(notification)
 }
--- server/main.go
+++ server/main.go
@@ -8,13 +8,21 @@ import (
 	"github.com/ddeville/kattungar_notify/store"
 )
 
+const teamId = "Q8B696Y8U4"
+const appId = "com.ddeville.kattungar_notify"
+
 func main() {
 	store, err := store.NewStore("/home/damien/Downloads/store.db")
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	apns, err := apns.NewApnsClient("/home/damien/Downloads/AuthKey_SZQY3SP3XB.p8")
+	apns, err := apns.NewApnsClient(apns.ApnsConfig{
+		TeamId:  teamId,
+		AppId:   appId,
+		KeyId:   "SZQY3SP3XB",
+		KeyPath: "/home/damien/Downloads/AuthKey_SZQY3SP3XB.p8",
+	})
 	if err != nil {
 		log.Fatal(err)
 	}
