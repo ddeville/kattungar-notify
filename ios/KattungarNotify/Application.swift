8193828ef9c1b4f5988a4e90123706a335b73b3d
Damien Deville <damien@ddeville.me>
2024-01-02 22:51:44 -0800

    Add ios project

---
+++ ios/KattungarNotify/Application.swift
@@ -0,0 +1,17 @@
+//
+//  Kattungar_NotifyApp.swift
+//  Kattungar Notify
+//
+//  Created by Damien Deville on 1/2/24.
+//
+
+import SwiftUI
+
+@main
+struct KattungarNotifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
