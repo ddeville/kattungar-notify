e134f7e07884b39347759f8d619d37fa0fc0f6a3
Damien Deville <damien@ddeville.me>
2024-01-02 18:25:16 -0800

    Add new method to push a notification

--- server/apns/apns.go
+++ server/apns/apns.go
@@ -2,11 +2,15 @@ package apns
 
 import (
 	"github.com/sideshow/apns2"
+	"github.com/sideshow/apns2/payload"
 	"github.com/sideshow/apns2/token"
+
+	"github.com/ddeville/kattungar_notify/store"
 )
 
 const teamId = "Q8B696Y8U4"
 const keyId = "SZQY3SP3XB"
+const appId = "com.ddeville.kattungar_notify"
 
 type ApnsClient struct {
 	inner *apns2.Client
@@ -26,4 +30,12 @@ func NewApnsClient(keyPath string) (*ApnsClient, error) {
 
 	client := apns2.NewTokenClient(token)
 	return &ApnsClient{client}, nil
+}
+
+func (c *ApnsClient) Push(device store.Device, payload *payload.Payload) (*apns2.Response, error) {
+	notification := &apns2.Notification{}
+	notification.Topic = appId
+	notification.DeviceToken = device.Token
+	notification.Payload = payload
+	return c.inner.Push(notification)
 }
